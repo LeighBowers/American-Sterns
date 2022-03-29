fdba36a07353c79806e4a666199d59b677a586b8
Na-aimOverlord <91248779+Na-aim@users.noreply.github.com>
2022-03-29 11:46:40 +0200

    profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,42 +1,121 @@
 <template>
-  <div class="container">
-    <header class="jumbotron">
-      <h3>
-        <strong>{{ currentUser.name }}</strong> Profile
-      </h3>
-    </header>
-    <p>
-      <strong>Token:</strong>
-      {{ currentUser.accessToken.substring(0, 20) }} ...
-      {{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}
-    </p>
-    <p>
-      <strong>Id:</strong>
-      {{ currentUser.id }}
-    </p>
-    <p>
-      <strong>Email:</strong>
-      {{ currentUser.email }}
-    </p>
-    <strong>Authorities:</strong>
-    <ul>
-      <li v-for="role in currentUser.roles" :key="role">{{ role }}</li>
-    </ul>
+  <div class="containerers">
+    <div class="card">
+       <div class="img-name">
+        <img
+          src="https://i.postimg.cc/VN51NPys/man-svgrepo-com-1.png"
+          alt="profilna"
+        />
+        <h2>{{ currentUser.username }}</h2>
+       </div>
+      <div class="followers">
+        <p>
+          <strong>Token:</strong>
+          {{ currentUser.accessToken.substring(0, 20) }} ...
+          {{
+            currentUser.accessToken.substr(currentUser.accessToken.length - 20)
+          }}
+        </p>
+        <p>
+          <strong>Id:</strong>
+          {{ currentUser.id }}
+        </p>
+        <p>
+          <strong>Email:</strong>
+          {{ currentUser.email }}
+        </p>
+      </div>
+      <div class="button-follow"></div>
+      <div class="auth">
+        <strong>Authorities:</strong>
+        
+          <p v-for="role in currentUser.roles" :key="role">{{ role }}</p>
+       
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "Profile",
-  //   call data from other file like localStorage
   computed: {
     currentUser() {
       return this.$store.state.auth.user;
     },
   },
   mounted() {
-    if (!this.currentUsers) {
+    if (!this.currentUser) {
       this.$router.push("/login");
     }
   },
 };
 </script>
+<style scoped>
+.containerers {
+  padding-top: 100px;
+  padding-bottom: 100px;
+}
+.card {
+  width: 350px;
+  height: fit-content;
+  border-radius: 16px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 3px solid #89d8f0;
+  background: #1C86EE;
+  color: rgb(255, 255, 255);
+  -webkit-box-shadow: 0 0 50px #89d8f0;
+	        box-shadow: 0 0 50px #89d8f0;
+}
+.img-name {
+  width: 100%;
+  height: 210px;
+  margin-left: auto;
+  margin-right: auto;
+  border-bottom: 5px solid #89d8f0;
+  padding-top: 10px;
+}
+.img-name img {
+  display: block;
+  width: 140px;
+  height: 140px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 70px;
+  border: 5px solid #89d8f0;
+  background-color: rgb(234, 241, 247);
+}
+.auth {
+  text-align: center;
+  text-decoration: none;
+  list-style: none;
+}
+.img-name h2 {
+  text-align: center;
+}
+.followers {
+  height: 140px;
+  padding-top: 50px;
+  text-align: center;
+}
+.followers h3 {
+  text-align: center;
+}
+.button-follow {
+  height: 40px;
+}
+.button-follow button {
+  display: block;
+  width: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 16px;
+  background-color: rgb(182, 182, 233);
+  border-radius: 10px;
+  font-size: 18px;
+}
+.button-follow button:hover {
+  background-color: rgb(180, 180, 226);
+}
+
+</style>
